c24443c128c73ef89acdd26cd4ab8923ce5ad246
AquaRius129 <forcadoaleaann@gmail.com>
2025-09-18 12:50:03 +0800

    Add files via upload

---
+++ l.php
@@ -0,0 +1,20 @@
+<?php
+$conn=mysqli_connect("localhost","root","","book");
+
+if(!$conn){
+    die("Invalid");
+}
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+$sql="SELECT* FROM  staff WHERE $email='email' AND $password='password'";
+$result=mysqli_query($conn,$sql);
+
+if(mysqli_num_rows($result)>0){
+    echo"success"
+    exit();
+}else{
+    echo"try again";
+}
+mysqli_close($conn);
+?>
